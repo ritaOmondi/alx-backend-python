5e1918f158ca6d317aa6f265f821e13ffeaa19f6
ritaOmondi <cherylritaomondi@gmail.com>
2024-08-26 09:22:11 +0000

    method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,6 +16,22 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc"),
     ])
     @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test the public_repos method returns the expected list of repos"""
+    expected_repos = ['repo1', 'repo2', 'repo3']
+    mock_get_json.return_value = [
+        {'name': 'repo1'}, {'name': 'repo2'}, {'name': 'repo3'}
+    ]
+
+    with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+        mock_public_repos_url.return_value = 'https://api.github.com/orgs/test_org/repos'
+        client = GithubOrgClient('test_org')
+        self.assertEqual(client.public_repos(), expected_repos)
+
+    mock_get_json.assert_called_once_with('https://api.github.com/orgs/test_org/repos')
+    mock_public_repos_url.assert_called_once()
+    """
+    @patch('client.get_json')
     def test_org(self, org_name, mock_json):
         """ Test method returns correct output """
         endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
@@ -26,6 +42,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ("random-url", {'repos_url': 'http://some_url.com'})
     ])
+    """
     def test_public_repos_url(self, name, result):
         """ Test method returns correct output """
         with patch('client.GithubOrgClient.org',
